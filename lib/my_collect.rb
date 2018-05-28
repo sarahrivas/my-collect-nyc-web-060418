eb0369abc7d878cb0509f504d2a40a7754ab37e4
Sarah Rivas <sarah.r.rivas@gmail.com>
2018-05-28 18:47:52 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,7 +1,7 @@
 def my_collect(array)
   i = 0
-  modified_array = while i < array.length
-    yield(array[i])
+  while i < array.length
+   modified_array = yield(array[i])
     i+=1
   end  
   modified_array
